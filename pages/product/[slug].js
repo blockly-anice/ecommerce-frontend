ee6033b5eb924273432389ff46321d1f1f0f51be
blockly-anice <stepanyanhayk25@gmail.com>
2023-01-13 18:57:32 +0000

    product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,34 +1,47 @@
 import { useQuery } from "urql";
 import { GET_PRODUCT_QUERY } from "../../lib/query";
+import { useRouter } from "next/router";
+import { ProductDetailsStyle, ProductInfo, Quantity } from "../../styles/ProductDetails";
 
 
 export default function ProductDetails() {
   //example slug - cable-knit-jumper
+
+  //fetch slug
+  const { query } = useRouter();
+
+  //fetch graphql data
   const [results] = useQuery({
     query: GET_PRODUCT_QUERY,
-    variables: {slug: 'cable-knit-jumper' },
+    variables: {slug: query.slug },
   });
 
   const { data, fetching, error } = results;
   //check for data
   if (fetching) return <p>LOADING</p>;
   if (error) return <p>Uh oh, something went wrong. {error.message}</p>
-  if (data) console.log(data);
+  if (data) console.log({data}, 'do you give me dataaaa');
+
+  const {title,description,image} = data.products.data[0].attributes;
+
 
   return(
-    <div>
-    <img src="" alt="" />
+    <ProductDetailsStyle>
+    <img src={image.data.attributes.formats.medium.url} alt={title} />
+    <ProductInfo>
     <div>
       <h3>title</h3>
       <p>description</p>
+     <Quantity>
+        <span>quantity</span>
+        <button>+</button>
+        <p>0</p>
+        <button>-</button>
+      </Quantity>
     </div>
-    <div>
-      <span>quantity</span>
-      <button>+</button>
-      <p>0</p>
-      <button>-</button>
-    </div>
-    <button>add to cart</button>
-    </div>
+    <Buy>add to cart</Buy>
+    </ProductInfo>
+
+    </ProductDetailsStyle>
   )
 }
